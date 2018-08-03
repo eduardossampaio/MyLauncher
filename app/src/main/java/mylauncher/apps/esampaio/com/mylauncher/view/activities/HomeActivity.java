0088fa5d01531aeec80847a1478a9314f9d6beae
Eduardo Soares <eduardo.ssampaio@outlook.com>
2018-08-03 19:46:34 -0300

    feat: Optimizations
    Solving overdraw problems

--- app/src/main/java/mylauncher/apps/esampaio/com/mylauncher/view/activities/HomeActivity.java
+++ app/src/main/java/mylauncher/apps/esampaio/com/mylauncher/view/activities/HomeActivity.java
@@ -1,44 +1,42 @@
 package mylauncher.apps.esampaio.com.mylauncher.view.activities;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.graphics.PorterDuff;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.provider.Settings;
+import android.support.constraint.ConstraintLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.ImageSwitcher;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ViewSwitcher;
 
-import com.huanhailiuxin.coolviewpager.CoolViewPager;
-import com.huanhailiuxin.coolviewpager.adapter.LoopPagerAdapterWrapper;
-import com.squareup.picasso.Picasso;
-
 import java.util.ArrayList;
-import java.util.List;
 
-import id.zelory.compressor.Compressor;
+import mylauncher.apps.esampaio.com.mylauncher.R;
 import mylauncher.apps.esampaio.com.mylauncher.core.entities.Application;
 import mylauncher.apps.esampaio.com.mylauncher.core.packages.PackageUtils;
 import mylauncher.apps.esampaio.com.mylauncher.view.adapters.apps.AppsListPageAdapter;
-import mylauncher.apps.esampaio.com.mylauncher.R;
-import mylauncher.apps.esampaio.com.mylauncher.view.custom.InfinitePagerAdapter;
 import mylauncher.apps.esampaio.com.mylauncher.view.util.BitmapUtils;
-import mylauncher.apps.esampaio.com.mylauncher.view.wallpaper.WallpaperService;
 
-public class HomeActivity extends AppCompatActivity implements ViewPager.OnPageChangeListener{
+public class HomeActivity extends AppCompatActivity implements ViewPager.OnPageChangeListener {
 
     private ViewPager appsViewPager;
     private Bitmap[] backgroundsBitmaps;
     private ImageSwitcher imageSwitcher;
+    private Toolbar toolbar;
+    private ConstraintLayout rootLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,6 +45,10 @@ public class HomeActivity extends AppCompatActivity implements ViewPager.OnPageC
         ArrayList<Application> applications = PackageUtils.getInstalledPackages(this);
         appsViewPager = findViewById(R.id.vp);
         imageSwitcher = findViewById(R.id.slide_trans_imageswitcher);
+        rootLayout = findViewById(R.id.root_layout);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        setTitle(getString(R.string.my_apps));
         imageSwitcher.setFactory(new ViewSwitcher.ViewFactory() {
             public View makeView() {
                 ImageView myView = new ImageView(getApplicationContext());
@@ -64,15 +66,49 @@ public class HomeActivity extends AppCompatActivity implements ViewPager.OnPageC
         //temporario
         backgroundsBitmaps = new Bitmap[2];
         int quality = 600;
-        backgroundsBitmaps[0] = BitmapUtils. decodeSampledBitmapFromResource(getResources(), R.drawable.wallpaper5, quality, quality);
-        backgroundsBitmaps[1] = BitmapUtils. decodeSampledBitmapFromResource(getResources(), R.drawable.wallpaper6, quality, quality);
+        backgroundsBitmaps[0] = BitmapUtils.decodeSampledBitmapFromResource(getResources(), R.drawable.wallpaper5, quality, quality);
+        backgroundsBitmaps[1] = BitmapUtils.decodeSampledBitmapFromResource(getResources(), R.drawable.wallpaper6, quality, quality);
         setBackgroundImage(0);
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        super.onPrepareOptionsMenu(menu);
+
+        MenuItem item = menu.findItem(R.id.menu_item_settings);
+        Drawable icon = item.getIcon();
+        icon.setColorFilter(getResources().getColor(R.color.icons), PorterDuff.Mode.SRC_IN);
+
+        item.setIcon(icon);
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.home_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.menu_item_settings) {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        return false;
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        Log.d("HomeActivity","onResume");
         this.appsViewPager.getAdapter().notifyDataSetChanged();
+    }
+
+    @Override
+    public void onBackPressed() {
+
     }
 
     @Override
@@ -85,20 +121,20 @@ public class HomeActivity extends AppCompatActivity implements ViewPager.OnPageC
         setBackgroundImage(position);
     }
 
-    private void setBackgroundImage(final int position){
+    private void setBackgroundImage(final int position) {
 
         new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
             @Override
             public void run() {
 
                 try {
-            int realPosition = position % 2;
-            imageSwitcher.setImageDrawable(new BitmapDrawable(getResources(),backgroundsBitmaps[realPosition]));
-        }catch (Exception e){
+                    int realPosition = position % 2;
+                    rootLayout.setBackground(new BitmapDrawable(getResources(), backgroundsBitmaps[realPosition]));
+                } catch (Exception e) {
 
-        }
+                }
             }
-        },100);
+        }, 400);
 
     }
 
